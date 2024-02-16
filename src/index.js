e19b79934e1eb58f40bd73e1bc88d544c0fcf9fd
SnehaManohar23 <snemanoh@iu.edu>
2024-02-15 20:25:06 -0500

    add

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const {add} =require("./arithmetica")
+const { add, subtract, multiply, divide } = require("./arithmetica");
 const app = express();
 const port = 3001;
 
